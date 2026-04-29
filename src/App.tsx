31f180262c68397ced812fe6de10400863a22eec
Eterea Studio <info@etereastudio.it>
2026-04-29 16:29:19 +0200

    chore: rimuovi commento temporaneo

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Footer } from './components/layout/Footer';
 import { Hero } from './components/sections/Hero';
 import { Services } from './components/sections/Services';
 import { Works } from './components/sections/Works';
-// import { Apps } from './components/sections/Apps'; // Temporaneamente disabilitato
+// import { Apps } from './components/sections/Apps';
 import { About } from './components/sections/About';
 import { Contact } from './components/sections/Contact';
 import { FAQ } from './components/sections/FAQ';
@@ -170,7 +170,7 @@ function App() {
                 <Hero />
                 <Services />
                 <Works />
-                {/* <Apps /> */} // Sezione "Le nostre app" temporaneamente disabilitata
+                {/* <Apps /> */}
                 <About />
                 <FAQ />
                 <Contact onOpenPrivacy={() => setActiveLegalPage('privacy')} />
